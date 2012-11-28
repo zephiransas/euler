c62acb69041ec8ef763ca98ada588e4911852b5b
zephiransas <zephiransas@gmail.com>
2012-11-28 17:01:03 +0900

    Fix problem3
    is_divide method not treat 2 as a prime

--- problem3.rb
+++ problem3.rb
@@ -1,6 +1,7 @@
 def is_prime(n)
-  return false if n < 2 || n % 2 == 0
+  return false if n < 2
   return true  if n == 2
+  return false if n % 2 == 0
   i = 3
   while i * i <= n
     return false if n % i == 0
