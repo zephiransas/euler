ef3ca6a139a44b5d24f289b6a25f0603116a8cc0
zephiransas <zephiransas@gmail.com>
2012-11-29 09:52:27 +0900

    Answer problem2

--- problem2.rb
+++ problem2.rb
@@ -1,7 +1,15 @@
 def fib(n)
   return 1 if n <= 1
-  return 2 if n = 2
+  return 2 if n == 2
   fib(n - 2) + fib(n - 1)
 end
 
-
+arr = []
+ix = 0
+while true
+  ix = ix + 1
+  v = fib(ix)
+  break if v > 4000000
+  arr << v
+end
+arr.select{|v|v % 2 == 0}.inject(0){|sum, i|sum = sum + i}
